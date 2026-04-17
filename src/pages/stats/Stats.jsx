96e719bd2479d7b130d5f9d66a7f43ee896aff4b
NilaRaniNath <nilunath335@gmail.com>
2026-04-17 23:24:15 +0600

    readme file added

--- src/pages/stats/Stats.jsx
+++ src/pages/stats/Stats.jsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TimelineContext } from '../../components/context/TimelineContext';
+import { Legend, Pie, PieChart, Tooltip } from 'recharts';
+// import { RechartsDevtools } from '@recharts/devtools';
 
 const Stats = () => {
+
+    const { timeline } = useContext(TimelineContext); 
+
+      const totalText = timeline.filter((it) => it.type === "text").length;
+  const totalCall = timeline.filter((i) => i.type === "call").length;
+  const totalVideo = timeline.filter((i) => i.type === "video").length;
+
+  const isEmpty = totalText === 0 && totalCall === 0 && totalVideo === 0;
+
+  const data = isEmpty
+    ? [
+        { name: "Text", value: 1, realValue: 0 },
+        { name: "Call", value: 1, realValue: 0 },
+        { name: "Video", value: 1, realValue: 0 },
+      ]
+    : [
+        { name: "Text", value: totalText, realValue: totalText },
+        { name: "Call", value: totalCall, realValue: totalCall },
+        { name: "Video", value: totalVideo, realValue: totalVideo },
+      ];
+
+
+
+
+
     return (
-        <div>
-            Stats page
+        <div className='flex items-center justify-center min-h-screen bg-gray-100 p-6' >
+          <div className='w-full max-w-5xl rounded-xl shadow-sm p-8'>
+              <div className='mt-5'>
+                <h2 className='font-bold text-3xl'>Friendship Analytics</h2>
+            </div>
+
+            <div className='shadow-sm rounded-lg p-10'>
+                <h3>By Interaction Type</h3>
+            
+             <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '80vh',margin:'auto', aspectRatio: 1 }} responsive>
+      <Pie
+        data={data}
+        innerRadius="80%"
+        outerRadius="100%"
+        // Corner radius is the rounded edge of each pie slice
+        cornerRadius="50%"
+        fill="#8884d8"
+        // padding angle is the gap between each pie slice
+        paddingAngle={5}
+        dataKey="value"
+        isAnimationActive={true}
+      />
+      {/* <RechartsDevtools /> */}
+      <Legend></Legend>
+      <Tooltip></Tooltip>
+    </PieChart>
+        </div>
+          </div>
         </div>
     );
 };
